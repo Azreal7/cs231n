ffcbda61f1e64175a7f054c82188953243448738
郭毅 <96096039+Azreal7@users.noreply.github.com>
2023-10-18 00:23:26 +0800

    完成svm支持向量机代码

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -34,17 +34,20 @@ def svm_loss_naive(W, X, y, reg):
         for j in range(num_classes):
             if j == y[i]:
                 continue
+            # svm将数据得分分为“分类正确”和“分类错误”两类。
             margin = scores[j] - correct_class_score + 1  # note delta = 1
             if margin > 0:
                 loss += margin
+                dW[:,y[i]] += -X[i]
+                dW[:,j] += X[i]
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
-
+    dW /= num_train
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
-
+    dW += 2 * reg * W
     #############################################################################
     # TODO:                                                                     #
     # Compute the gradient of the loss function and store it dW.                #
@@ -54,6 +57,7 @@ def svm_loss_naive(W, X, y, reg):
     # code above to compute the gradient.                                       #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
 
     pass
 
@@ -97,4 +101,22 @@ def svm_loss_vectorized(W, X, y, reg):
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
+    num_train= X.shape[0]  # N
+    num_classes = W.shape[1]  # C
+    scores = X.dot(W)  # N by C
+    scores_correct = scores[np.arange(num_train), y]  # 1 by N broadcasting
+    scores_correct = np.reshape(scores_correct, (num_train, 1)) # N by 1
+    margins = scores - scores_correct + 1.0
+    margins[np.arange(num_train), y] = 0.0
+    margins[margins<=0] = 0.0 #
+    loss += np.sum(margins) / num_train
+    loss += 0.5 * reg *np.sum(W * W)
+
+    #compute the gradient
+    margins[margins>0] = 1.0
+    row_sum = np.sum(margins, axis=1)
+    margins[np.arange(num_train), y] = -row_sum
+    dW += np.dot(X.T, margins)/num_train + reg* W
+
+    
     return loss, dW
